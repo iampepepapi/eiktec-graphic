f2a27ca18c5e98e8fc789a1ff6ab4eb8ac8ddbcb
pepepapi <108593422+pepepapi@users.noreply.github.com>
2024-06-12 07:42:44 +0200

    kinde vercel env work?5

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import React from "react";
 
 const Dashboard = () => {
-    return <div>Dashboard Hello2</div>;
+    return <div>Dashboard Hello3</div>;
 };
 
 export default Dashboard;
